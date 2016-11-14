6e1a99030d6093ade8112d404d9621b6289205fe
Boro Vukovic <TheBoroer@users.noreply.github.com>
2016-11-14 17:25:21 -0500

    Added path to project for GIT_REPULL commands

--- scripts/start.sh
+++ scripts/start.sh
@@ -51,10 +51,10 @@ if [ ! -d "/var/www/html/.git" ]; then
  fi
 else
  if [ ! -z "$GIT_REPULL" ]; then
-   git rm -r --quiet --cached /var/www/html
-   git fetch --all -p
-   git reset HEAD --quiet
-   git pull
+   git -C /var/www/html rm -r --quiet --cached /var/www/html
+   git -C /var/www/html fetch --all -p
+   git -C /var/www/html reset HEAD --quiet
+   git -C /var/www/html pull
    chown -Rf nginx.nginx /var/www/html
  fi
 fi
